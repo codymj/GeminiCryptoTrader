18d76f0030aeda849e1c6fdc2468dadb4ee38089
Cody Johnson <codyj@protonmail.com>
2018-01-23 10:21:03 -0500

    Compartmentalize MainWindow

--- src/GeminiCryptoTrader.py
+++ src/GeminiCryptoTrader.py
@@ -9,7 +9,7 @@ from MainWindow import MainWindow
 from PyQt5 import QtWidgets
 import sys
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
--- src/MainWindow.py
+++ src/MainWindow.py
@@ -6,10 +6,12 @@
 ################################################################################
 
 import sys
+from ui_MainWindow import Ui_MainWindow
 from PyQt5 import QtGui, QtWidgets, uic
 
 class MainWindow(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        uic.loadUi('MainWindow.ui', self)
+        ui = Ui_MainWindow()
+        ui.setupUi(self)
         self.show()
