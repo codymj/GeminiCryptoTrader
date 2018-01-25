8506648b78cc2785cce993ef05d588566fc5eece
Cody Johnson <codyj@protonmail.com>
2018-01-25 15:42:30 -0500

    Create SellDialog

---
+++ src/SellDialog.py
@@ -0,0 +1,24 @@
+################################################################################
+#
+#  SellDialog.py
+#  Author: Cody Johnson <codyj@protonmail.com>
+#
+################################################################################
+
+import sys
+from ui_BuyDialog import Ui_BuyDialog
+from PyQt5 import uic, QtGui, QtWidgets
+from PyQt5.QtCore import pyqtSlot
+
+class SellDialog(QtWidgets.QDialog):
+    def __init__(self, parent):
+        super(SellDialog, self).__init__(parent)
+        self.initUI()
+
+    def initUI(self):
+        ui = Ui_SellDialog()
+        ui.setupUi(self)
+
+        # Connect actions
+        ui.cancelButton.clicked.connect(self.close)
+        #ui.okButton.clicked.connect(self.saveAccountInfoToFile)
