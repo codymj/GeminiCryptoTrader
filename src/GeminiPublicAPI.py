995072ebb17ce33acc5e47ed165345578e4ba30d
Cody Johnson <codyj@protonmail.com>
2018-02-26 16:05:28 -0500

    Rename class to Ticker

--- src/GeminiPublicAPI.py
+++ src/GeminiPublicAPI.py
@@ -10,40 +10,24 @@
 import json, datetime, urllib
 from urllib.request import urlopen
 
-class MarketData:
+class Ticker:
     # Class data
-    baseUrl = ''        # Base URL
-    account = {}        # Account data
+    baseUrl = 'https://api.gemini.com/v1/'
+
     symbols = []        # List of symbols (ex. 'btcusd')
-    tickers = []        # Ticker data
+    btcusdTicker = {}
+    ethusdTicker = {}
 
     # Initializer
-    def __init__(self, account):
-        # Set account
-        self.account = account
-
-        # Set base URL
-        if self.account['isSandbox']:
-            self.baseUrl = 'https://api.sandbox.gemini.com/v1/'
-        else:
-            self.baseUrl = 'https://api.gemini.com/v1/'
-
-        # Build tickers structure
-        btcusd = {}
-        ethusd = {}
-        ethbtc = {}
-        self.tickers.append(btcusd)     # tickers[0] = btcusd data
-        self.tickers.append(ethusd)     # tickers[1] = ethusd data
-        self.tickers.append(ethbtc)     # tickers[2] = ethbtc data
-
-        # Get data from Gemini
-        self.symbols = self.getSymbols()
+    def __init__(self):
+        self.getSymbols()
+        self.updateTickers()
 
     # Gets list of symbols from Gemini: ["btcusd", "ethusd", "ethbtc"]
     ############################################################################
     def getSymbols(self):
         response = urlopen(self.baseUrl + '/symbols')
-        return json.loads(response.read())
+        self.symbols = json.loads(response.read())
 
     # Updates ticker data
     ############################################################################
@@ -52,14 +36,19 @@ class MarketData:
             if symbol == 'btcusd':
                 postUrl = "/pubticker/btcusd"
                 response = urlopen(self.baseUrl + postUrl)
-                self.tickers[0] = json.loads(response.read())
+                self.btcusdTicker = json.loads(response.read())
             elif symbol == 'ethusd':
                 postUrl = "/pubticker/ethusd"
                 response = urlopen(self.baseUrl + postUrl)
-                self.tickers[1] = json.loads(response.read())
-            elif symbol == 'ethbtc':
-                postUrl = "/pubticker/ethbtc"
-                response = urlopen(self.baseUrl + postUrl)
-                self.tickers[2] = json.loads(response.read())
+                self.ethusdTicker = json.loads(response.read())
+            #elif symbol == 'ethbtc':
+                #postUrl = "/pubticker/ethbtc"
+                #response = urlopen(self.baseUrl + postUrl)
+                #self.tickers[2] = json.loads(response.read())
             else:
-                return
+                continue
+
+    # Sends data to main window
+    ############################################################################
+    def getData(self):
+        return (self.btcusdTicker, self.ethusdTicker)
